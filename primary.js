1ac069e42e0a8877522288bd9bc09cd031b526ee
Matt Tilley <mtilley255@gmail.com>
2017-08-03 11:03:48 -0500

    CHAPTER 2
    Completed chapter 2.

--- primary.js
+++ primary.js
@@ -1,4 +1,15 @@
 /*THESE ENTRIES ARE PRIMARILY FOR PRACTICE STUFF
 RELATED TO "A SMARTER WAY TO LEARN JAVASCRIPT"*/
 
-alert("Hello World!");
+
+var name = "Matt";
+
+for (i=0; i<name.length; i++)
+	{
+		alert(name);
+	}
+
+/*var name = "Matt";
+console.log(name);*/
+
+/*alert("Hello World!");*/
