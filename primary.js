db7632dab8ac377d624136bca832de3120b6a3ca
Matt Tilley <mtilley255@gmail.com>
2017-08-03 14:45:13 -0500

    CHAPTER 10
    Skipped ahead / completed chapter 10.

--- primary.js
+++ primary.js
@@ -1,6 +1,25 @@
 /*THESE ENTRIES ARE PRIMARILY FOR PRACTICE STUFF
 RELATED TO "A SMARTER WAY TO LEARN JAVASCRIPT"*/
 
+var answer = "tail";
+console.log(answer);
+
+var response = prompt("What does a dog wag?");
+
+if (response == "tail" || response == "Tail" || response == "TAIL")
+{
+	alert("Damn right.");
+}
+
+else
+	{
+		alert("Nope.");
+	}
+
+
+
+
+/*CHAPTER 3
 var another_num;
 another_num = 30;
 another_num += 12;
@@ -8,12 +27,11 @@ another_num++;
 another_num--;
 console.log(another_num);
 
-
 var num = 42;
 num = num + 8;
 console.log(num);
 console.log(another_num + num);
-
+*/
 
 
 
