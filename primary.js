fb57ed01eeeaea6910080e1ba2cb6cf4157f595b
Matt Tilley <mtilley255@gmail.com>
2017-08-03 11:48:43 -0500

    CHAPTER 3
    Completed chapter 3 exercises.

--- primary.js
+++ primary.js
@@ -1,15 +1,34 @@
 /*THESE ENTRIES ARE PRIMARILY FOR PRACTICE STUFF
 RELATED TO "A SMARTER WAY TO LEARN JAVASCRIPT"*/
 
+var another_num;
+another_num = 30;
+another_num += 12;
+another_num++;
+another_num--;
+console.log(another_num);
 
+
+var num = 42;
+num = num + 8;
+console.log(num);
+console.log(another_num + num);
+
+
+
+
+/*CHAPTER 2
 var name = "Matt";
 
 for (i=0; i<name.length; i++)
 	{
 		alert(name);
-	}
+	}*/
 
 /*var name = "Matt";
 console.log(name);*/
 
-/*alert("Hello World!");*/
+
+
+/*CHAPTER 1
+alert("Hello World!");*/
